1ecfb4a365a6dca1520cf617162a2a83000a35be
Ryan Wycoff <70543363+ryanwycoff@users.noreply.github.com>
2023-03-11 19:45:33 -0500

    renamed and styled python files

--- audio_processing.py
+++ audio_processing.py
@@ -1,59 +1,68 @@
-import python_speech_features
-import scipy
-from scipy.io.wavfile import read
-import matplotlib.pyplot as plt
-from python_speech_features import mfcc
-import numpy as np
+import csv
 import os
-import tqdm
-from sklearn import preprocessing
-import ffmpeg
-from pydub import AudioSegment
 import subprocess
-from pydub.silence import split_on_silence
+
+import acrcloud
+import ffmpeg
+import IPython
+import matplotlib.pyplot as plt
+import numpy as np
+import python_speech_features
+import requests
+import scipy
 import torch
 import torchaudio
-import IPython
-import requests
-import acrcloud
-import csv
+import tqdm
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from python_speech_features import mfcc
+from scipy.io.wavfile import read
+from sklearn import preprocessing
+
 # use python_speech_features for MFCC
+
+
 def genMFCC(filepath):
     samplerate, signal = read(filepath)
     mfcc_features = mfcc(signal,
                          samplerate,
-                         winlen = 0.05,
-                         winstep = 0.01,
-                         numcep = 10,
-                         nfft = 1024)
+                         winlen=0.05,
+                         winstep=0.01,
+                         numcep=10,
+                         nfft=1024)
     mfcc_features = preprocessing.scale(mfcc_features)
     return mfcc_features
+
 
 def splitAudio(filepath, id, t1, t2):
     newAudio = AudioSegment.from_wav(filepath)
     newAudio = newAudio[t1:t2]
-    newAudio.export(str(id)+'.wav', format="wav") #Exports to a wav file in the current path.
+    # Exports to a wav file in the current path.
+    newAudio.export(str(id)+'.wav', format="wav")
+
 
 def loadAudio(filepath):
     newAudio = AudioSegment.from_wav(filepath)
     print("Done!")
 
+
 def getName(filename):
     config = {
-	    "key": "7dcf14702f55380d375bd9c62904bb1a",
-	    "secret": "m00m5kbIXvPxaupxYfFu8MWtPrqeUZMKbnwyLFt3",
-	    "host": "https://identify-us-west-2.acrcloud.com"
+        "key": "7dcf14702f55380d375bd9c62904bb1a",
+        "secret": "m00m5kbIXvPxaupxYfFu8MWtPrqeUZMKbnwyLFt3",
+        "host": "https://identify-us-west-2.acrcloud.com"
     }
     audio = filename
     acr = acrcloud.ACRcloud(config)
     name = acr.recognizer(audio)
     return name
 
-def getNames(len = 2000):
-    with open('DEAM_names.csv', 'w',encoding='utf-8') as f:
+
+def getNames(len=2000):
+    with open('data/DEAM_names.csv', 'w', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(['artist','genre','spotify_id'])
-        for i in range(2,len):
+        writer.writerow(['artist', 'genre', 'spotify_id'])
+        for i in range(2, len):
             print(i)
             filename = str(i)+".wav"
             if os. path. isfile(filename):
@@ -65,51 +74,58 @@ def getNames(len = 2000):
                 savid = ""
                 if 'spotify' in data['metadata']['music'][0]['external_metadata']:
                     savid = data['metadata']['music'][0]['external_metadata']['spotify']['track']['id']
-                #print(data['metadata']['music'][0])
+                # print(data['metadata']['music'][0])
                 print("written")
                 writer.writerow(
                     [data['metadata']['music'][0]['artists'][0]['name'],
                      data['metadata']['music'][0]['genres'][0]['name'][:10],
                      savid
                      ])
+
+
 def str_to_val(s):
     if s[0] != '-':
         return float(s)
     return -float(s[1:])
-def read_csv(filename, type, restrict = 2000):
-    cnt =0
+
+
+def read_csv(filename, type, restrict=2000):
+    cnt = 0
     all_set = []
-    with open('id_record.txt','r',encoding='utf-8') as f0:
+    with open('data/id_record.txt', 'r', encoding='utf-8') as f0:
         for ord in f0:
             all_set.append(int(ord))
-    with open(type+'.txt', 'w',encoding='utf-8') as f:
-        with open(filename,'r',encoding='utf-8') as source:
+    with open(type+'.txt', 'w', encoding='utf-8') as f:
+        with open(filename, 'r', encoding='utf-8') as source:
             for row in source:
-                if cnt>=restrict:
+                if cnt >= restrict:
                     break
                 cnt += 1
-                if cnt==1:
+                if cnt == 1:
                     continue
                 data = row.split(',')
                 for i in range(len(data)):
                     data[i] = str_to_val(data[i])
-                data[0] = int(data[0]) # the id
+                data[0] = int(data[0])  # the id
                 if not data[0] in all_set:
                     continue
-                values = [sum(data[2:22])/10, sum(data[22:42])/10, sum(data[42:62])/10]
-                #output = str(data[0]) + ' '+str(values[0]) + ' ' + str(values[1]) + ' '+ str(values[2])
-                #output = output + '\n'
-                #f.write(output)
+                values = [sum(data[2:22])/10, sum(data[22:42]) /
+                          10, sum(data[42:62])/10]
+                # output = str(data[0]) + ' '+str(values[0]) + ' ' + str(values[1]) + ' '+ str(values[2])
+                # output = output + '\n'
+                # f.write(output)
                 f.write(str(values[0])+'\n')
                 f.write(str(values[1])+'\n')
                 f.write(str(values[2])+'\n')
     pass
-#print("test")
-#cur_mfcc = genMFCC('test-real.wav')
-#splitAudio('test-real.wav',0,15)
-#loadAudio('fortest.wav')
-#print(genMFCC('fortest.wav'))
-#print(getName('fortest3.wav')['status']['msg'])
-#getNames(len)
-read_csv('data/arousal.csv','arousal')
-read_csv('data/valence.csv','valence')
+
+
+# print("test")
+# cur_mfcc = genMFCC('test-real.wav')
+# splitAudio('test-real.wav',0,15)
+# loadAudio('fortest.wav')
+# print(genMFCC('fortest.wav'))
+# print(getName('fortest3.wav')['status']['msg'])
+# getNames(len)
+read_csv('data/arousal.csv', 'arousal')
+read_csv('data/valence.csv', 'valence')
