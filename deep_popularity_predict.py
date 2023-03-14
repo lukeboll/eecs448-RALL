2ad445e4302cca9626f4db25dacdfdf79362a521
Ryan Wycoff <70543363+ryanwycoff@users.noreply.github.com>
2023-03-13 21:20:03 -0400

    new data on deep learning model

--- deep_popularity_predict.py
+++ deep_popularity_predict.py
@@ -4,24 +4,25 @@ from  keras.layers import Input, Dense, Dropout
 from sklearn.model_selection import train_test_split
 
 # Load the data
-valence_arousal_data = pd.read_csv('data/csv/muse_v3.csv', header=0)
-valence_arousal_data = valence_arousal_data[['track', 'valence_tags', 'arousal_tags']]
-valence_arousal_data = valence_arousal_data[valence_arousal_data['track'].notnull()]
-valence_arousal_data = valence_arousal_data[valence_arousal_data['valence_tags'].notnull()]
-valence_arousal_data = valence_arousal_data[valence_arousal_data['arousal_tags'].notnull()]
+# valence_arousal_data = pd.read_csv('data/csv/muse_v3.csv', header=0)
+# valence_arousal_data = valence_arousal_data[['track', 'valence_tags', 'arousal_tags']]
+# valence_arousal_data = valence_arousal_data[valence_arousal_data['track'].notnull()]
+# valence_arousal_data = valence_arousal_data[valence_arousal_data['valence_tags'].notnull()]
+# valence_arousal_data = valence_arousal_data[valence_arousal_data['arousal_tags'].notnull()]
 
-spotify_data = pd.read_csv('data/csv/song_data.csv', header=0)
-spotify_data = spotify_data[['song_name', 'song_popularity']]
+# spotify_data = pd.read_csv('data/csv/song_data.csv', header=0)
+# spotify_data = spotify_data[['song_name', 'song_popularity']]
 
-merged_df = pd.merge(valence_arousal_data, spotify_data, left_on='track', right_on='song_name', how='inner')
-merged_df.drop('song_name', axis=1, inplace=True)
-merged_df = merged_df.drop_duplicates(subset=['track'])
-merged_df.drop('track', axis=1, inplace=True)
-merged_df = merged_df.rename(columns={'valence_tags': 'valence', 'arousal_tags': 'arousal' , 'song_popularity': 'popularity'})
+# merged_df = pd.merge(valence_arousal_data, spotify_data, left_on='track', right_on='song_name', how='inner')
+# merged_df.drop('song_name', axis=1, inplace=True)
+# merged_df = merged_df.drop_duplicates(subset=['track'])
+# merged_df.drop('track', axis=1, inplace=True)
+# merged_df = merged_df.rename(columns={'valence_tags': 'valence', 'arousal_tags': 'arousal' , 'song_popularity': 'popularity'})
 
-print(merged_df)
+# print(merged_df)
+merged_df = pd.read_csv('data/csv/prediction_dataset.csv')
  
-X = merged_df[['valence', 'arousal']].values
+X = merged_df[['valence_tags', 'arousal_tags']].values
 y = merged_df['popularity'].values
 
 # Split the data into training and testing sets
@@ -47,7 +48,7 @@ model = tf.keras.Model(inputs=inputs, outputs=outputs)
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-history = model.fit(x_train, y_train, epochs=100, validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=10, validation_split=0.2)
 
 # Save the model
 # model.save('data/models/popularity_model.h5')
@@ -62,3 +63,6 @@ test_loss = model.evaluate(x_test, y_test)
 
 # Make predictions
 predictions = model.predict(x_test)
+
+print(test_loss)
+print(predictions)
