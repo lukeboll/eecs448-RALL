6d623fc950ab94639ce50cdd80c4bcbc738cf576
Luke Boll <lboll@sandia.gov>
2023-03-15 11:50:39 -0400

    Save

--- lyrics/prelim_model.py
+++ lyrics/prelim_model.py
@@ -1,26 +1,27 @@
 # Common libraries
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, GridSearchCV, train_test_split 
 import matplotlib.pyplot as plt
 
 # Linear regression Question libraries
 from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+
 
 # Logistic Regression Question Libraries
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import mean_squared_error
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 import seaborn as sns
 
-
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# Reading in the Spotify Data
+# Reading in the Spotify Data and conducting pre-processing
 df = pd.read_csv('spotify_songs.csv')
 
 new_df = pd.DataFrame({
@@ -48,4 +49,57 @@ def PreprocessData(new_df, nltk_stopwords):
 
 new_df = PreprocessData(new_df, nltk_stopwords)
 
-print(new_df.text)
+""" 
+Linear Model with Bag of Words 
+"""
+
+# Step 1: Prepare the data
+
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(new_df.text)
+y = new_df.label
+
+# Step 3: Data Transformation
+X = X.toarray()
+y = np.array(y)
+
+# Step 4: Model Training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=448)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Step 5: Model Evaluation
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+y_pred_train = model.predict(X_train)
+train_mse = mean_squared_error(y_train, y_pred_train)
+print("BoW Mean Squared Error:", mse)
+print("BoW Training Mean Squared Error:", train_mse)
+
+
+""" 
+Linear Model with Bag of Words 
+"""
+
+# Step 1: Prepare the data
+
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(new_df.text)
+y = new_df.label
+
+# Step 3: Data Transformation
+X = X.toarray()
+y = np.array(y)
+
+# Step 4: Model Training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=448)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Step 5: Model Evaluation
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+y_pred_train = model.predict(X_train)
+train_mse = mean_squared_error(y_train, y_pred_train)
+print("Tf-idf Mean Squared Error:", mse)
+print("Tf-idf Training Mean Squared Error:", train_mse)
