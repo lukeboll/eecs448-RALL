db0df4a99af145b4cc105c732ebe30b681860408
Luke Boll <lboll@sandia.gov>
2023-03-19 22:07:08 -0400

    Saving the bert progress

--- lyrics/model_bert.py
+++ lyrics/model_bert.py
@@ -2,6 +2,8 @@ import pandas as pd
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+from sklearn.model_selection import train_test_split
+import time
 
 # Load data from CSV file
 df = pd.read_csv('spotify_songs.csv')
@@ -12,6 +14,7 @@ df.dropna(inplace=True)
 # reset the index
 df.reset_index(drop=True, inplace=True)
 
+df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
 # Initialize BERT tokenizer and model
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
@@ -53,8 +56,10 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 print("Training has started")
 # Train BERT model
+start = time.time()
 for epoch in range(epochs):
-    print(f'Epoch {epoch + 1} of {epochs}')
+    timeTaken = time.time() - start
+    print(f'Epoch {epoch + 1} of {epochs}. Time is {timeTaken:.4f}')
     model.train()
     total_loss = 0
     for step, batch in enumerate(train_dataloader):
@@ -70,3 +75,38 @@ for epoch in range(epochs):
     print(f'Average training loss: {avg_loss}')
 
 torch.save(model.state_dict(), 'my_model.pth')
+
+# Create dataloader for testing set
+input_ids_test = []
+attention_masks_test = []
+labels_test = []
+for i in range(len(test_df)):
+    encoded_dict = tokenizer.encode_plus(test_df['lyrics'][i],
+                                          add_special_tokens=True,
+                                          max_length=128,
+                                          pad_to_max_length=True,
+                                          return_attention_mask=True,
+                                          return_tensors='pt')
+    input_ids_test.append(encoded_dict['input_ids'])
+    attention_masks_test.append(encoded_dict['attention_mask'])
+    labels_test.append(torch.tensor(test_df['track_popularity'][i], dtype=torch.float))
+input_ids_test = torch.cat(input_ids_test, dim=0)
+attention_masks_test = torch.cat(attention_masks_test, dim=0)
+labels_test = torch.stack(labels_test)
+
+dataset_test = TensorDataset(input_ids_test, attention_masks_test, labels_test)
+test_dataloader = DataLoader(dataset_test, sampler=SequentialSampler(dataset_test), batch_size=32)
+
+# Evaluate model on testing set
+model.eval()
+total_loss_test = 0
+with torch.no_grad():
+    for step, batch in enumerate(test_dataloader):
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, attention_mask, labels = batch
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        total_loss_test += loss.item()
+
+avg_loss_test = total_loss_test / len(test_dataloader)
+print(f'Average testing loss: {avg_loss_test}')
