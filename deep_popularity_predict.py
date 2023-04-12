d3a86e53cdf74e32781ad4a90b93bc555ce21f2c
Ryan Wycoff <70543363+ryanwycoff@users.noreply.github.com>
2023-04-11 22:35:52 -0400

    changed input features

--- deep_popularity_predict.py
+++ deep_popularity_predict.py
@@ -9,43 +9,33 @@ NUM_FEATURES = 11 # 10 input features, 1 output feature
 
 DATA = pd.read_csv('data/csv/prediction_dataset.csv', header=0)
 
-DATA = DATA[DATA['popularity'].notnull()]
-DATA = DATA[DATA['danceability'].notnull()]
-DATA = DATA[DATA['energy'].notnull()]
-DATA = DATA[DATA['loudness'].notnull()]
-DATA = DATA[DATA['speechiness'].notnull()]
-DATA = DATA[DATA['acousticness'].notnull()]
-DATA = DATA[DATA['instrumentalness'].notnull()]
-DATA = DATA[DATA['liveness'].notnull()]
-DATA = DATA[DATA['valence_tags'].notnull()]
-DATA = DATA[DATA['arousal_tags'].notnull()]
 DATA = DATA[DATA['genre'].notnull()]
+DATA = DATA[DATA['valence'].notnull()]
+DATA = DATA[DATA['arousal'].notnull()]
+DATA = DATA[DATA['bert_features'].notnull()]
+DATA = DATA[DATA['popularity'].notnull()]
+
+
 
 def get_genre_data(genre): # rock, pop, indie, soul, folk
     genre_data = DATA[DATA['genre'] == genre]
     X = genre_data[[
-        'danceability', 
-        'energy', 
-        'loudness', 
-        'speechiness',
-        'acousticness', 
-        'instrumentalness', 
-        'liveness',
-        'valence_tags', 
-        'arousal_tags'
+        'valence', 
+        'arousal', 
+        'bert_features'
     ]].astype(float).values
     y = genre_data['popularity'].astype(float).values
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=485)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=448)
 
     return X_train, X_test, y_train, y_test
 
-X_train_rock, X_test_rock, y_train_rock, y_test_rock = get_genre_data('rock')
+
 X_train_pop, X_test_pop, y_train_pop, y_test_pop = get_genre_data('pop')
-X_train_indie, X_test_indie, y_train_indie, y_test_indie = get_genre_data('indie')
-X_train_soul, X_test_soul, y_train_soul, y_test_soul = get_genre_data('soul')
-X_train_folk, X_test_folk, y_train_folk, y_test_folk = get_genre_data('folk')
+X_train_rock, X_test_rock, y_train_rock, y_test_rock = get_genre_data('rock')
+X_train_edm, X_test_edm, y_train_edm, y_test_edm = get_genre_data('edm')
+
 
 # Build the model
 model = Sequential([
@@ -62,34 +52,34 @@ model.compile(optimizer=Adam(learning_rate=0.001), loss='mse', metrics=['mae'])
 
 
 # Train the models
-rock_history = model.fit(X_train_rock, y_train_rock, validation_split=0.1, batch_size=32, epochs=100)
-pop_history = model.fit(X_train_pop, y_train_pop, validation_split=0.1, batch_size=32, epochs=100)
-indie_history = model.fit(X_train_indie, y_train_indie, validation_split=0.1, batch_size=32, epochs=100)
-soul_history = model.fit(X_train_soul, y_train_soul, validation_split=0.1, batch_size=32, epochs=100)
-folk_history = model.fit(X_train_folk, y_train_folk, validation_split=0.1, batch_size=32, epochs=100)
+pop_model = model.fit(X_train_pop, y_train_pop, validation_split=0.1, batch_size=32, epochs=100)
+rock_model = model.fit(X_train_rock, y_train_rock, validation_split=0.1, batch_size=32, epochs=100)
+edm_model = model.fit(X_train_edm, y_train_edm, validation_split=0.1, batch_size=32, epochs=100)
 
 
 # Save the model
-# model.save('data/models/popularity_model.h5')
+# pop_model.save('data/models/pop_popularity_model.h5')
+# rock_model.save('data/models/rock_popularity_model.h5')
+# edm_model.save('data/models/edm_popularity_model.h5')
 
 ### Testing ###
 
 # Load the model
-# model = tf.keras.models.load_model('data/models/popularity_model.h5')
+# pop_model = tf.keras.models.load_model('data/models/pop_popularity_model.h5')
+# rock_model = tf.keras.models.load_model('data/models/rock_popularity_model.h5')
+# edm_model = tf.keras.models.load_model('data/models/edm_popularity_model.h5')
 
 # Evaluate the model
-test_loss_rock, test_mae_rock = model.evaluate(X_test_rock, y_test_rock)
 test_loss_pop, test_mae_pop = model.evaluate(X_test_pop, y_test_pop)
-test_loss_indie, test_mae_indie = model.evaluate(X_test_indie, y_test_indie)
-test_loss_soul, test_mae_soul = model.evaluate(X_test_soul, y_test_soul)
-test_loss_folk, test_mae_folk = model.evaluate(X_test_folk, y_test_folk)
+test_loss_rock, test_mae_rock = model.evaluate(X_test_rock, y_test_rock)
+test_loss_edm, test_mae_edm = model.evaluate(X_test_edm, y_test_edm)
 
-print(f'Test MAE (ROCK): {test_mae_rock:.2f}')
 print(f'Test MAE (POP): {test_mae_pop:.2f}')
-print(f'Test MAE (INDIE): {test_mae_indie:.2f}')
-print(f'Test MAE (SOUL): {test_mae_soul:.2f}')
-print(f'Test MAE (FOLK): {test_mae_folk:.2f}')
-print(test_loss_rock, test_mae_rock)
+print(f'Test MAE (ROCK): {test_mae_rock:.2f}')
+print(f'Test MAE (EDM): {test_mae_edm:.2f}')
+
 # Make predictions
-# predictions = model.predict(X_test_rock)
+# pop_prediction = model.predict(X_test_pop)
+# rock_prediction = model.predict(X_test_rock)
+# edm_prediction = model.predict(X_test_edm)
 
